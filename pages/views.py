ae6ada8a2bcace14abf5ca6e3b77e68fa062223a
Serhat <mserhatbalik@gmail.com>
2026-04-19 15:41:11 +0300

    Implemented templates and context data

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home_page_view(request):
     return HttpResponse("Homepage")
+
+
+def about_page_view(request):
+    context = {"name": "Serhat", "age": 40}
+    return render(request, "pages/about.html", context)
